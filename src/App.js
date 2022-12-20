a181a5fc25acf4fcde174604f8baecbb00854f30
yenphung123 <121089818+yenphung123@users.noreply.github.com>
2022-12-20 19:44:53 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="title">
+      <input className="task" placeholder="Enter your tasks here..."></input>
+      <ul className="list">
+        <li>Clean up bedroom</li>
+        <li>Buy some milk</li>
+        <li>Jogging</li>
+        <li>Learn React</li>
+        <li>Doing exercise</li>
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
